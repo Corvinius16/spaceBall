e521c71eba4988ec08f6456df6efdcc2915295e2
Alexey Pavlov <corvinius16@gmail.com>
2021-02-28 15:11:29 +0300

    remove score

--- src/js/go/score.js
+++ src/js/go/score.js
@@ -13,17 +13,17 @@ class Score  extends GameObject{
     addScore(count){
         this.score +=count;
 
-        if(count!=1)
-        {
-        var width = window.innerWidth, height = window.innerHeight;
-        var widthHalf = width / 2, heightHalf = height / 2;
-        var pos = window.container.sphere.getThreeObject().position.clone();
-        pos.project(this.MainScene.getMainCamera().getThreeObject());
-        pos.x = ( pos.x * widthHalf ) + widthHalf;
-        pos.y = - ( pos.y * heightHalf ) + heightHalf;
+        // if(count!=1)
+        // {
+        // var width = window.innerWidth, height = window.innerHeight;
+        // var widthHalf = width / 2, heightHalf = height / 2;
+        // var pos = window.container.sphere.getThreeObject().position.clone();
+        // pos.project(this.MainScene.getMainCamera().getThreeObject());
+        // pos.x = ( pos.x * widthHalf ) + widthHalf;
+        // pos.y = - ( pos.y * heightHalf ) + heightHalf;
 
-        let html = new ScoreHtml(pos,count);
-        }
+        // let html = new ScoreHtml(pos,count);
+        // }
         // console.log(pos);
         this.updateHTML();
     }
