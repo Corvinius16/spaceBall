61afff499e0345ba2b27bc13bb06e94029ad55de
Alexey Pavlov <corvinius16@gmail.com>
2021-04-01 17:31:19 +0300

    remove favorite if favorite8

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ let tutorHtml = document.querySelector(".tutorGif");
 
 
 let addtoFav = document.querySelector("#addToFavorites");
-alert(JSON.stringify(parser.paramsAsObject));
+document.querySelector(".menuButtonContainer h1").innerHTML = JSON.stringify(parser.paramsAsObject);
 if(parser.getParam("vk_is_favorite") == "1"){
     addtoFav.classList.add("hide");
 }
